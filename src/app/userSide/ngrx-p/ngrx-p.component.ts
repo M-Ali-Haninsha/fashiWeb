7f1de838e5dfa332a8a18888bf7926f5e41380b5
M-Ali-Haninsha <alianinsha@gmail.com>
2024-01-01 09:34:35 +0530

    ngrxChange

--- src/app/userSide/ngrx-p/ngrx-p.component.ts
+++ src/app/userSide/ngrx-p/ngrx-p.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../states/app.state';
+import { selectCount } from '../states/counter/counter.selector';
+import { AsyncPipe } from '@angular/common';
+import { decrement, increment, reset } from '../states/counter/counter.actions';
+
 
 @Component({
   selector: 'app-ngrx-p',
+  standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './ngrx-p.component.html',
   styleUrls: ['./ngrx-p.component.css']
 })
 export class NgrxPComponent {
+  count$: Observable<number>;
 
+  constructor(private store: Store<AppState>) {
+    this.count$ = this.store.select(selectCount)
+  }
+
+  increment() {
+    this.store.dispatch(increment())
+  }
+
+  decrement() {
+    this.store.dispatch(decrement())
+  }
+
+  reset() {
+    this.store.dispatch(reset())
+  }
 }
